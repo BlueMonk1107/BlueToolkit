fe989c89483b14c6a51822b925811d3a8c75260d
Andy <1174200070@qq.com>
2019-01-13 14:21:44 +0800

    优化代码结构

---
+++ Assets/ToolKit/ExportObj/Editor/ExportFile.cs
@@ -0,0 +1,125 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace BlueToolkit
+{
+    public class ExportFile     
+    {
+        /// <summary>
+        /// 导出文件夹名称 路径为工程根目录
+        /// </summary>
+        public const string EXPORT_FOLDER = "ExportedObj";
+
+        //保存材质
+        private static void MaterialsToFile(Dictionary<string, MaterialData> materialList, string folder, string filename)
+        {
+            using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".mtl"))
+            {
+                foreach (KeyValuePair<string, MaterialData> kvp in materialList)
+                {
+                    sw.Write("\n");
+                    sw.Write("newmtl {0}\n", kvp.Key);
+                    sw.Write("Ka  0.6 0.6 0.6\n");
+                    sw.Write("Kd  0.6 0.6 0.6\n");
+                    sw.Write("Ks  0.9 0.9 0.9\n");
+                    sw.Write("d  1.0\n");
+                    sw.Write("Ns  0.0\n");
+                    sw.Write("illum 2\n");
+
+                    if (kvp.Value.TextureName != null)
+                    {
+                        string destinationFile = kvp.Value.TextureName;
+
+
+                        int stripIndex = destinationFile.LastIndexOf('/');
+
+                        if (stripIndex >= 0)
+                            destinationFile = destinationFile.Substring(stripIndex + 1).Trim();
+
+
+                        string relativeFile = destinationFile;
+
+                        destinationFile = folder + "/" + destinationFile;
+
+                        try
+                        {
+                            File.Copy(kvp.Value.TextureName, destinationFile);
+                        }
+                        catch
+                        {
+
+                        }
+
+
+                        sw.Write("map_Kd {0}", relativeFile);
+                    }
+
+                    sw.Write("\n\n\n");
+                }
+            }
+        }
+        /// <summary>
+        /// 导出单个模型
+        /// </summary>
+        /// <param name="mf"></param>
+        /// <param name="folder"></param>
+        /// <param name="filename"></param>
+        public static void ExportObj(MeshFilter mf, string folder, string filename)
+        {
+            Dictionary<string, MaterialData> materialList = new Dictionary<string, MaterialData>();
+            MeshData data = new MeshData(mf, materialList);
+
+            using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".obj"))
+            {
+                sw.Write("mtllib ./" + filename + ".mtl\n");
+
+                sw.Write(data.ToString());
+            }
+
+            MaterialsToFile(materialList, folder, filename);
+        }
+        /// <summary>
+        /// 导出多个模型
+        /// </summary>
+        /// <param name="mf"></param>
+        /// <param name="folder"></param>
+        /// <param name="filename"></param>
+        public static void ExportObjs(MeshFilter[] mf, string folder, string filename)
+        {
+            Dictionary<string, MaterialData> materialList = new Dictionary<string, MaterialData>();
+
+            using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".obj"))
+            {
+                sw.Write("mtllib ./" + filename + ".mtl\n");
+
+                foreach (MeshFilter mesh in mf)
+                {
+                    MeshData data = new MeshData(mesh, materialList);
+                    sw.Write(data.ToString());
+                }
+            }
+
+            MaterialsToFile(materialList, folder, filename);
+        }
+        /// <summary>
+        /// 创建导出目录
+        /// </summary>
+        /// <returns></returns>
+        public static bool CreateExportFolder()
+        {
+            try
+            {
+                System.IO.Directory.CreateDirectory(EXPORT_FOLDER);
+            }
+            catch
+            {
+                EditorUtility.DisplayDialog("错误", "创建导出文件夹失败", "关闭");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Assets/ToolKit/ExportObj/Editor/ExportObjMenu.cs
+++ Assets/ToolKit/ExportObj/Editor/ExportObjMenu.cs
@@ -11,14 +11,10 @@ namespace BlueToolkit
 {
     public class ExportObjMenu : ScriptableObject
     {
-        //导出文件夹名称 路径为工程根目录
-        private const string EXPORT_FOLDER = "ExportedObj";
-
-
         [MenuItem("BlueToolKit/导出模型/将选中模型分别导出（子物体会拆分导出）")]
         private static void ExportAllChild()
         {
-            if (!CreateExportFolder())
+            if (!ExportFile.CreateExportFolder())
                 return;
 
             Transform[] selection = Selection.GetTransforms(SelectionMode.Editable | SelectionMode.ExcludePrefab);
@@ -29,7 +25,7 @@ namespace BlueToolkit
                 return;
             }
 
-            int exportedObjects = 0;
+            int exportCount = 0;
 
             for (int i = 0; i < selection.Length; i++)
             {
@@ -37,13 +33,13 @@ namespace BlueToolkit
 
                 for (int m = 0; m < meshfilter.Length; m++)
                 {
-                    exportedObjects++;
-                    MeshToFile((MeshFilter)meshfilter[m], EXPORT_FOLDER, selection[i].name + "_" + i + "_" + m);
+                    exportCount++;
+                    ExportFile.ExportObj((MeshFilter)meshfilter[m], ExportFile.EXPORT_FOLDER, selection[i].name + "_" + i + "_" + m);
                 }
             }
 
-            if (exportedObjects > 0)
-                EditorUtility.DisplayDialog("导出成功", "成功导出 " + exportedObjects + " 个模型", "关闭");
+            if (exportCount > 0)
+                EditorUtility.DisplayDialog("导出成功", "成功导出 " + exportCount + " 个模型", "关闭");
             else
                 EditorUtility.DisplayDialog("导出失败", "导出模型必须含有Mesh Filter组件", "关闭");
         }
@@ -51,7 +47,7 @@ namespace BlueToolkit
         [MenuItem("BlueToolKit/导出模型/将选中模型导出成一个obj文件")]
         private static void ExportToSingleObj()
         {
-            if (!CreateExportFolder())
+            if (!ExportFile.CreateExportFolder())
                 return;
 
 
@@ -63,7 +59,7 @@ namespace BlueToolkit
                 return;
             }
 
-            int exportedObjects = 0;
+            int exportCount = 0;
 
             ArrayList mfList = new ArrayList();
 
@@ -73,12 +69,12 @@ namespace BlueToolkit
 
                 for (int m = 0; m < meshfilter.Length; m++)
                 {
-                    exportedObjects++;
+                    exportCount++;
                     mfList.Add(meshfilter[m]);
                 }
             }
 
-            if (exportedObjects > 0)
+            if (exportCount > 0)
             {
                 MeshFilter[] mf = new MeshFilter[mfList.Count];
 
@@ -87,14 +83,14 @@ namespace BlueToolkit
                     mf[i] = (MeshFilter)mfList[i];
                 }
 
-                string filename = SceneManager.GetActiveScene() + "_" + exportedObjects;
+                string filename = SceneManager.GetActiveScene() + "_" + exportCount;
 
                 int stripIndex = filename.LastIndexOf('/'); //FIXME: Should be Path.PathSeparator
 
                 if (stripIndex >= 0)
                     filename = filename.Substring(stripIndex + 1).Trim();
 
-                MeshesToFile(mf, EXPORT_FOLDER, filename);
+                ExportFile.ExportObjs(mf, ExportFile.EXPORT_FOLDER, filename);
 
 
                 EditorUtility.DisplayDialog("导出成功", "导出模型名称：" + filename, "关闭");
@@ -106,7 +102,7 @@ namespace BlueToolkit
         [MenuItem("BlueToolKit/导出模型/将选中模型分别导出（子物体不拆分导出）")]
         private static void ExportParent()
         {
-            if (!CreateExportFolder())
+            if (!ExportFile.CreateExportFolder())
                 return;
 
             Transform[] selection = Selection.GetTransforms(SelectionMode.Editable | SelectionMode.ExcludePrefab);
@@ -117,7 +113,7 @@ namespace BlueToolkit
                 return;
             }
 
-            int exportedObjects = 0;
+            int exportCount = 0;
 
 
             for (int i = 0; i < selection.Length; i++)
@@ -131,117 +127,17 @@ namespace BlueToolkit
                     mf[m] = (MeshFilter)meshfilter[m];
                 }
 
-                exportedObjects++;
+                exportCount++;
 
-                MeshesToFile(mf, EXPORT_FOLDER, selection[i].name + "_" + i);
+                ExportFile.ExportObjs(mf, ExportFile.EXPORT_FOLDER, selection[i].name + "_" + i);
             }
 
-            if (exportedObjects > 0)
+            if (exportCount > 0)
             {
-                EditorUtility.DisplayDialog("导出成功", "成功导出 " + exportedObjects + " 个模型", "关闭");
+                EditorUtility.DisplayDialog("导出成功", "成功导出 " + exportCount + " 个模型", "关闭");
             }
             else
                 EditorUtility.DisplayDialog("导出失败", "导出模型必须含有Mesh Filter组件", "关闭");
         }
-
-        //保存材质
-        private static void MaterialsToFile(Dictionary<string, MaterialData> materialList, string folder, string filename)
-        {
-            using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".mtl"))
-            {
-                foreach (KeyValuePair<string, MaterialData> kvp in materialList)
-                {
-                    sw.Write("\n");
-                    sw.Write("newmtl {0}\n", kvp.Key);
-                    sw.Write("Ka  0.6 0.6 0.6\n");
-                    sw.Write("Kd  0.6 0.6 0.6\n");
-                    sw.Write("Ks  0.9 0.9 0.9\n");
-                    sw.Write("d  1.0\n");
-                    sw.Write("Ns  0.0\n");
-                    sw.Write("illum 2\n");
-
-                    if (kvp.Value.TextureName != null)
-                    {
-                        string destinationFile = kvp.Value.TextureName;
-
-
-                        int stripIndex = destinationFile.LastIndexOf('/');
-
-                        if (stripIndex >= 0)
-                            destinationFile = destinationFile.Substring(stripIndex + 1).Trim();
-
-
-                        string relativeFile = destinationFile;
-
-                        destinationFile = folder + "/" + destinationFile;
-
-                        try
-                        {
-                            File.Copy(kvp.Value.TextureName, destinationFile);
-                        }
-                        catch
-                        {
-
-                        }
-
-
-                        sw.Write("map_Kd {0}", relativeFile);
-                    }
-
-                    sw.Write("\n\n\n");
-                }
-            }
-        }
-        //保存单个模型
-        private static void MeshToFile(MeshFilter mf, string folder, string filename)
-        {
-            Dictionary<string, MaterialData> materialList = new Dictionary<string, MaterialData>();
-            MeshData data = new MeshData(mf, materialList);
-
-            using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".obj"))
-            {
-                sw.Write("mtllib ./" + filename + ".mtl\n");
-
-                sw.Write(data.ToString());
-            }
-
-            MaterialsToFile(materialList, folder, filename);
-        }
-        //保存多个模型
-        private static void MeshesToFile(MeshFilter[] mf, string folder, string filename)
-        {
-            Dictionary<string, MaterialData> materialList = new Dictionary<string, MaterialData>();
-
-            using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".obj"))
-            {
-                sw.Write("mtllib ./" + filename + ".mtl\n");
-
-                foreach (MeshFilter mesh in mf)
-                {
-                    MeshData data = new MeshData(mesh, materialList);
-                    sw.Write(data.ToString());
-                }
-            }
-
-            MaterialsToFile(materialList, folder, filename);
-        }
-        //创建导出目录
-        private static bool CreateExportFolder()
-        {
-            try
-            {
-                System.IO.Directory.CreateDirectory(EXPORT_FOLDER);
-            }
-            catch
-            {
-                EditorUtility.DisplayDialog("错误", "创建导出文件夹失败", "关闭");
-                return false;
-            }
-
-            return true;
-        }
-
-
-
     }
 }
